156fb8184969c1e8b0167567a2d310623eaf54df
uliana <avdeewa.ul@yandex.ru>
2019-03-11 13:48:58 +0300

    meta tags

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="style.css" rel="stylesheet">
     <link href="adaptive.css" rel="stylesheet">
 </head>
