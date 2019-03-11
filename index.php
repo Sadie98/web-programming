27cd818d483496299119c5eb4004a2550527fd57
uliana <avdeewa.ul@yandex.ru>
2019-03-11 11:58:23 +0300

    plus css

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+<div>
+    <ol class="menu">
+        <li>Моя странца</li>
+        <li>Мои друзья</li>
+        <li>Сообщения</li>
+        <li>Друзья</li>
+        <li>Группы</li>
+        <li>Фотографии</li>
+        <li>Музыка</li>
+    </ol>
+</div>
+</body>
+</html>
