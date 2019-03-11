37f4ae53493f2c43261dacc8eb4a7c297795010f
uliana <avdeewa.ul@yandex.ru>
2019-03-11 13:07:34 +0300

    beauty makeup

--- index.php
+++ index.php
@@ -5,16 +5,25 @@
     <link href="style.css" rel="stylesheet">
 </head>
 <body>
-<div>
-    <ol class="menu">
-        <li>Моя странца</li>
-        <li>Мои друзья</li>
-        <li>Сообщения</li>
-        <li>Друзья</li>
-        <li>Группы</li>
-        <li>Фотографии</li>
-        <li>Музыка</li>
-    </ol>
+<div class="main-page">
+    <div class="left-column">
+        <div class="menu"></div>
+    </div>
+    <div class="center-column">
+        <div class="page-photo">
+            <div class="photo"></div>
+            <div class="edit-buttons">
+                <div class="edit-profile"></div>
+                <div class="stats"></div>
+            </div>
+        </div>
+        <div class="gifts"></div>
+    </div>
+    <div class="right-column">
+        <div class="about"></div>
+        <div class="add-news"></div>
+        <div class="wall"></div>
+    </div>
 </div>
 </body>
 </html>
